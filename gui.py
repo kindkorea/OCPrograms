1c4791c9a317eb196310d4dc4b4fcce678c7efb2
Kenneth <kindkorea@gmail.com>
2023-12-14 15:50:30 +0900

    add function

--- gui.py
+++ gui.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 import gui_pdf2jpg
 import gui_faxReceive
+import gui_calculator
+
 
 class App(tk.Tk):
     def __init__(self):
@@ -27,8 +29,10 @@ class App(tk.Tk):
         input_frame = gui_pdf2jpg.Pdf2jpg(self,8)
         input_frame.grid(column=0, row=0, pady = 20)
         fax_receive = gui_faxReceive.GUI_FaxReceive(self)
-        fax_receive.grid(column=0, row=1)
-
+        fax_receive.grid(column=0, row=1, pady = 20)
+        calc = gui_calculator.Calculator(self)
+        calc.grid(column=0, row=2)
+        
 
 if __name__ == "__main__":
     app = App()
--- test_data.py
+++ test_data.py
@@ -1,5 +1,67 @@
-a = 1
 
-if a == 0 or a == 1:
-    print("ok")
+import math
+
+class MarginCalc():
+    def __init__(self,buy_cost):
+        self.data_list = ['매입가','공급가','마진율(%)','마진금','부가세','판매가']
+        self.calc_data = {
+            'buy_cost' : [ '매입가', 0],
+            'margin_rate' : [ '공급가', 0],
+            'price' : [ '마진율(%)', 0],
+            'price_tax' : [ '마진금', 0],
+            'price_wtax' : [ '부가세', 0],
+            'margin' : [ '판매가', 0]
+        }
+        
+        
+        
+        
+    def reset(self):
+        for ix in self.calc_data.values():
+            print(ix)
+        
     
+    def calc_by_margin_rate(self,margin_rate):
+        self.reset()
+        self.margin_rate = margin_rate
+        self.price = round(self.buy_cost / (1-(margin_rate/100)))
+        self.price_tax = round(self.price * 0.1)
+        self.price_wtax = self.price + self.price_tax
+        self.margin = self.price - self.buy_cost
+        
+    def calc_by_margin(self,margin):
+        self.reset()
+        self.margin = margin
+        self.price = round(self.buy_cost + self.margin)
+        self.margin_rate = round((self.margin / self.price) * 100)
+        self.price_tax = round(self.price * 0.1)
+        self.price_wtax = self.price + self.price_tax
+        
+        
+    def calc_by_price(self,price):
+        self.reset()
+        self.price = price
+        self.margin =  self.price  - self.buy_cost
+        self.margin_rate = round((self.margin / self.price) * 100)
+        self.price_tax = round(self.price * 0.1)
+        self.price_wtax = self.price + self.price_tax
+        
+    def marginCalc_print(self):
+        print(f'{self.buy_cost=}')
+        print(f'{self.margin_rate=}')
+        print(f'{self.price=}')
+        print(f'{self.price_tax=}')
+        print(f'{self.price_wtax=}')
+        print(f'{self.margin=}')
+        print('')
+        
+
+app = MarginCalc(10000)
+app.calc_by_margin_rate(20)
+app.marginCalc_print()
+
+app.calc_by_margin(2500)
+app.marginCalc_print()
+
+app.calc_by_price(12500)
+app.marginCalc_print()
